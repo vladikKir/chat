c67cafa4400700aa2020d479c482bad855313a2a
vladikKir <vlad@Vlad.localdomain>
2022-12-13 23:31:29 +0300

    eslint fixes

--- frontend/src/routes/error_page.jsx
+++ frontend/src/routes/error_page.jsx
@@ -1,16 +1,14 @@
-import React from "react";
-import NavBar from "../components/navBar";
+import React from 'react';
+import NavBar from '../components/navBar';
 
-const ErrorPage = () => {
-	return (
-			<>
-				<NavBar />
-				<div className="text-center"  id="error-page" style={{padding: "20px"}}>
-						<img src="/pictures/error_emoji.svg" alt="" width="50%" height="50%" />
-						<h1>Oops, page didn`t found</h1>
-				</div>
-			</>
-	)
-}
+const ErrorPage = () => (
+  <>
+    <NavBar />
+    <div className="text-center" id="error-page" style={{ padding: '20px' }}>
+      <img src="/pictures/error_emoji.svg" alt="" width="50%" height="50%" />
+      <h1>Oops, page didn`t found</h1>
+    </div>
+  </>
+);
 
 export default ErrorPage;
--- frontend/src/routes/login/login.jsx
+++ frontend/src/routes/login/login.jsx
@@ -1,11 +1,9 @@
-import React from "react";
-import { object, string } from "yup";
+import React from 'react';
+import { object, string } from 'yup';
 import { useFormik } from 'formik';
-import NavBar from "../../components/navBar";
-
+import NavBar from '../../components/navBar';
 
 const LoginPage = () => {
-
   const formik = useFormik({
     initialValues: {
       username: '',
@@ -21,8 +19,8 @@ const LoginPage = () => {
     }),
   });
 
-	return (
-		<div className="h-100" id="chat">
+  return (
+    <div className="h-100" id="chat">
       <div className="d-flex flex-column h-100">
         <NavBar />
         <div className="container-fluid h-100">
@@ -31,20 +29,20 @@ const LoginPage = () => {
               <div className="card shadow-sm">
                 <div className="card-body row p-5">
                   <div className="col-12 col-md-6 d-flex align-items-center justify-content-center">
-                    <img src="/pictures/chat_form.svg" className="rounded-circle" alt="Войти"></img>
+                    <img src="/pictures/chat_form.svg" className="rounded-circle" alt="Войти" />
                   </div>
-                    <form className="col-12 col-md-6 mt-3 mt-mb-0">
-                      <h1 className="text-center mb-4">Войти</h1>
-                      <div className="form-floating mb-3">
-                        <input name="username" autoComplete="username" required="" placeholder="Ваш ник" id="username" className="form-control" onChange={formik.handleChange} value={formik.values.username} />
-                        <label htmlFor="username">Ваш ник</label>
-                      </div>
-                      <div className="form-floating mb-4">
-                        <input name="password" autoComplete="current-password" required="" placeholder="Пароль" type="password" id="password" className="form-control" onChange={formik.handleChange} value={formik.values.password} />
-                        <label className="form-label" htmlFor="password">Пароль</label>
-                      </div>
-                      <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
-                    </form>
+                  <form className="col-12 col-md-6 mt-3 mt-mb-0">
+                    <h1 className="text-center mb-4">Войти</h1>
+                    <div className="form-floating mb-3">
+                      <input name="username" autoComplete="username" required="" placeholder="Ваш ник" id="username" className="form-control" onChange={formik.handleChange} value={formik.values.username} />
+                      <label htmlFor="username">Ваш ник</label>
+                    </div>
+                    <div className="form-floating mb-4">
+                      <input name="password" autoComplete="current-password" required="" placeholder="Пароль" type="password" id="password" className="form-control" onChange={formik.handleChange} value={formik.values.password} />
+                      <label className="form-label" htmlFor="password">Пароль</label>
+                    </div>
+                    <button type="submit" className="w-100 mb-3 btn btn-outline-primary">Войти</button>
+                  </form>
                 </div>
                 <div className="card-footer p-4">
                   <div className="text-center">
@@ -57,9 +55,9 @@ const LoginPage = () => {
           </div>
         </div>
       </div>
-      <div className="Toastify"></div>
-		</div>
-	)
-}
+      <div className="Toastify" />
+    </div>
+  );
+};
 
 export default LoginPage;
