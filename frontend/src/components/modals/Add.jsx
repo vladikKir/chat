ee54ba27413d74dc913491147cfbd193f1c9e1ec
vladikKir <vlad@Vlad.localdomain>
2023-01-03 23:40:55 +0300

    Fix Add closebutton

--- frontend/src/components/modals/Add.jsx
+++ frontend/src/components/modals/Add.jsx
@@ -2,10 +2,11 @@ import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { useFormik } from 'formik';
 import { Modal, FormGroup, FormControl } from 'react-bootstrap';
+import { object, string } from 'yup';
 import { addModal } from '../../slices/modal';
 import useSocket from '../../hooks/useSocket';
 
-const Add = ({ updateModal }) => {
+const Add = () => {
   const dispatch = useDispatch();
   const socket = useSocket();
 
@@ -18,6 +19,12 @@ const Add = ({ updateModal }) => {
     initialValues: {
       body: '',
     },
+    validationSchema: object({
+      body: string()
+        .min(1, 'Must be at least 1 character')
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+    }),
     onSubmit: ({ body }) => handleSubmit(body),
   });
 
@@ -28,17 +35,17 @@ const Add = ({ updateModal }) => {
   });
 
   return (
-    <Modal show onSubmit={formik.handleSubmit} onHide={() => updateModal('unactive')}>
+    <Modal show onSubmit={formik.handleSubmit} onHide={() => dispatch(addModal('unactive'))}>
       <Modal.Header>
-        <Modal.Title>Add</Modal.Title>
-        <button type="button" className="btn-close" aria-label="Close" onClick={() => updateModal('unactive')} />
+        <Modal.Title>Добавить канал</Modal.Title>
+        <button type="button" className="btn-close" aria-label="Close" onClick={() => dispatch(addModal('unactive'))} />
       </Modal.Header>
       <Modal.Body>
         <form>
           <FormGroup>
             <FormControl ref={inputEl} id="body" onChange={formik.handleChange} value={formik.values.body} />
+            <input className="btn btn-primary" type="submit" value="submit" />
           </FormGroup>
-          <input className="btn btn-primary" type="submit" value="submit" />
         </form>
       </Modal.Body>
     </Modal>
