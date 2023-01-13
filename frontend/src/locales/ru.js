f304da61bbf5be48ce78711edf2a0961b41934e5
vladikKir <vlad@Vlad.localdomain>
2023-01-13 12:31:52 +0300

    Fix channel control label

--- frontend/src/locales/ru.js
+++ frontend/src/locales/ru.js
@@ -30,7 +30,7 @@ export default {
       },
       channels: {
         dropdown: {
-          channelControl: 'Управление каналов',
+          channelControl: 'Управление каналом',
           delete: 'Удалить',
           rename: 'Переименовать',
         },
